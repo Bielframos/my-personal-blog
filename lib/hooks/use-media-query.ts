93459eeef1f5faf1d8f60ce429aa94b66a19025d
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-03-13 02:49:34 -0300

    use media query hook added

---
+++ lib/hooks/use-media-query.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+function removeReservedMediaKeyWord(mediaQueryString: string) {
+  return mediaQueryString.replace("@media", "").trim()
+}
+
+export const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(removeReservedMediaKeyWord(query))
+    const handler = (event: MediaQueryListEvent) => setMatches(event.matches)
+    setMatches(mediaQuery.matches)
+
+    mediaQuery.addEventListener("change", handler)
+
+    return () => {
+      mediaQuery.removeEventListener("change", handler)
+    }
+  }, [query])
+
+  return matches
+}
