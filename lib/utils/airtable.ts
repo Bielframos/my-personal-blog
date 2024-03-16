c5850135ddef32e0f793fdc66d9dedde73df861e
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-03-16 01:54:19 -0300

    first attempt to fix build error

--- lib/utils/airtable.ts
+++ lib/utils/airtable.ts
@@ -1,5 +1,4 @@
 import Airtable from "airtable"
-import { unstable_cache } from "next/cache"
 
 const API_KEY = process.env.AIRTABLE_API_KEY
 
@@ -7,46 +6,42 @@ export const db = new Airtable({
   apiKey: API_KEY,
 }).base("appYDe7xI4LmVnNc0")
 
-export const getGames = unstable_cache(
-  async function () {
-    return new Promise<{ [key: string]: Game[] }>((resolve, reject) => {
-      const games: { [key: string]: Game[] } = {}
+export async function getGames(): Promise<{ [key: string]: Game[] }> {
+  return new Promise((resolve, reject) => {
+    const games: { [key: string]: Game[] } = {}
 
-      db("zerei")
-        .select({
-          view: "zerados",
-          fields: ["name", "launchYear", "rating", "platform", "finishedAt", "yearRank"],
-        })
-        .eachPage(
-          (records, fetchNextPage) => {
-            records.forEach((record) => {
-              const game = record.fields as Game
-              if (game.finishedAt) {
-                const year = new Date(game.finishedAt).getFullYear().toString()
-                if (!(year in games)) {
-                  games[year] = []
-                }
-                games[year].push(game)
-              } else {
-                if (!("rest" in games)) {
-                  games["rest"] = []
-                }
-                games["rest"].push(game)
+    db("zerei")
+      .select({
+        view: "zerados",
+        fields: ["name", "launchYear", "rating", "platform", "finishedAt", "yearRank"],
+      })
+      .eachPage(
+        (records, fetchNextPage) => {
+          records.forEach((record) => {
+            const game = record.fields as Game
+            if (game.finishedAt) {
+              const year = new Date(game.finishedAt).getFullYear().toString()
+              if (!(year in games)) {
+                games[year] = []
               }
-            })
-            fetchNextPage()
-          },
-          (err) => {
-            if (err) {
-              console.error(err)
-              reject(err)
+              games[year].push(game)
             } else {
-              resolve(games)
+              if (!("rest" in games)) {
+                games["rest"] = []
+              }
+              games["rest"].push(game)
             }
+          })
+          fetchNextPage()
+        },
+        (err) => {
+          if (err) {
+            console.error(err)
+            reject(err)
+          } else {
+            resolve(games)
           }
-        )
-    })
-  },
-  ["airtable-games"],
-  { tags: ["games"] }
-)
+        }
+      )
+  })
+}
