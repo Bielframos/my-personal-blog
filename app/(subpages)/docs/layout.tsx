442588474e2f4e4852efefb543ab7a023a978b72
Gabriel França <gabrielfrancaramos99@gmail.com>
2025-01-23 09:30:48 -0300

    corrige erro do uso de cookies sem `Suspense` ou `use cache`

--- app/(subpages)/docs/layout.tsx
+++ app/(subpages)/docs/layout.tsx
@@ -1,7 +1,6 @@
 import { Card } from "@/lib/components/ui"
 
-export default function DocsLayout({
-	children,
-}: { children: React.ReactNode }) {
-	return <Card className="p-6 grid auto-rows-min gap-4">{children}</Card>
+export default function DocsLayout({ children }: { children: React.ReactNode }) {
+  return <Card className="p-6 grid auto-rows-min gap-4">{children}</Card>
 }
+
